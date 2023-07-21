8234049eb185fc10a232a44eb7761de04b39e0d1
Oleksii Petrashov <oleksii.petrashov@gmail.com>
2023-07-21 17:02:05 +0300

    Initial DB schema; README updated.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# picturest
-REST API for working with pictures in a cloud
+# PictuREST
+REST API for working with pictures in a cloud.
+
+1. Clone the repository and navigate to its root directory:  
+`git clone git@github.com:oleverse/picturest.git`  
+`cd picturest`  
+2. Create virtual environment and activate it:
+- with `poetry`:  
+`poetry shell`  
+- with `venv`:  
+`python -m venv .venv`  
+`source .venv/bin/activate`
+3. Install packages:  
+`poetry update`  
+or  
+`pip install -r requirements.txt`
+4. Copy file `env_sample` to `.env` and change values to fit your needs  
+5. Run `postgres` in `docker` container:  
+`docker-compose up -d`  
+5. Generate migrations with `alembic`:  
+`alembic revision --autogenerate -m 'Init'`
+6. Run migrations:  
+`alembic upgrade head`
--- api/database/models.py
+++ api/database/models.py
@@ -1,34 +1,119 @@
-from sqlalchemy import Column, Integer, String, func, ForeignKey, UniqueConstraint, Boolean
+import enum
+
+from sqlalchemy import Column, Integer, String, func, ForeignKey, Boolean,Table
 from sqlalchemy.orm import relationship, declarative_base
-from sqlalchemy.sql.sqltypes import Date, DateTime
+from sqlalchemy.sql.sqltypes import DateTime
 
 Base = declarative_base()
 
 
-class Contact(Base):
-    __tablename__ = "contacts"
-    __table_args__ = (
-        UniqueConstraint('email', 'user_id', name='unique_contact_user'),
-    )
+class RoleNames(enum.Enum):
+    admin: str = 'admin'
+    moderator: str = 'moderator'
+    user: str = 'user'
+
+    @staticmethod
+    def get_max_role_len():
+        return len(max(list(RoleNames.__members__), key=lambda item: len(item)))
+
+
+class Role(Base):
+    __tablename__ = "roles"
     id = Column(Integer, primary_key=True)
-    first_name = Column(String(100), nullable=False)
-    last_name = Column(String(100), nullable=False)
-    email = Column(String(150), nullable=False)
-    phone = Column(String(13), nullable=True)
-    birth_date = Column(Date, default=func.now())
-    created_at = Column('created_at', DateTime, default=func.now())
-    description = Column(String(250), nullable=True)
-    user_id = Column('user_id', ForeignKey('users.id', ondelete='CASCADE'), default=None)
-    user = relationship('User', backref="contacts")
+    name = Column(String(RoleNames.get_max_role_len()), default=RoleNames.user)
+    can_post_own_pict = Column(Boolean, default=True)
+    can_del_own_pict = Column(Boolean, default=True)
+    can_mod_own_pict = Column(Boolean, default=True)
+
+    can_post_own_comment = Column(Boolean, default=True)
+    can_mod_own_comment = Column(Boolean, default=True)
+    can_del_own_comment = Column(Boolean, default=False)
+
+    can_post_tag = Column(Boolean, default=True)
+    can_mod_tag = Column(Boolean, default=False)
+    can_del_tag = Column(Boolean, default=False)
+
+    can_post_not_own_pict = Column(Boolean, default=False)
+    can_mod_not_own_pict = Column(Boolean, default=False)
+    can_del_not_own_pict = Column(Boolean, default=False)
+
+    can_post_not_own_comment = Column(Boolean, default=False)
+    can_mod_not_own_comment = Column(Boolean, default=False)
+    can_del_not_own_comment = Column(Boolean, default=False)
+
+#    base_role_id = Column(Integer, ForeignKey("roles.id"), nullable=True)
+
+
+# TODO: 'user' role is the base one, 'moderator' and 'admin' should be based on 'user' but with laveraged privileges
+# @event.listens_for(Role.base_role_id, 'set')
+# def base_role_validation(target, value, oldvalue, initiator):
+#     return value if target.id != value else None
 
 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
-    username = Column(String(50))
-    email = Column(String(150), nullable=False, unique=True)
+    username = Column(String(100))
+    email = Column(String(100), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
-    created_at = Column('crated_at', DateTime, default=func.now())
-    avatar = Column(String(255), nullable=True)
     refresh_token = Column(String(255), nullable=True)
+    role_id = Column(Integer, ForeignKey(Role.id, ondelete="CASCADE"))
     confirmed = Column(Boolean, default=False)
+    is_active = Column(Boolean, default=False)
+    slug = Column(String(255), unique=True, nullable=False)
+    avatar = Column(String(1024), nullable=True)
+    created_at = Column(DateTime, default=func.now(), onupdate=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+
+
+picture_m2m_tag = Table(
+    "picture_m2m_tag",
+    Base.metadata,
+    Column("id", Integer, primary_key=True),
+    Column("picture_id", Integer, ForeignKey("pictures.id", ondelete="CASCADE")),
+    Column("tag_id", Integer, ForeignKey("tags.id", ondelete="CASCADE")),
+)
+
+
+class Tag(Base):
+    __tablename__ = "tags"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), unique=True)
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+
+
+class Picture(Base):
+    __tablename__ = "pictures"
+    id = Column(Integer, primary_key=True, index=True)
+    picture_url = Column(String(1024))
+    description = Column(String(10000))
+    user_id = Column('user_id', Integer, ForeignKey('users.id', ondelete="CASCADE"))
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+    update = Column(Boolean, default=False)
+    tags = relationship("Tag", secondary=picture_m2m_tag, backref="pictures")
+    user = relationship("User", backref="pictures")
+
+
+class TransformedPicture(Base):
+    __tablename__ = 'transformed_pictures'
+    id = Column(Integer, primary_key=True)
+    url = Column(String, nullable=False)
+    picture_id = Column(Integer, ForeignKey(Picture.id, ondelete="CASCADE"))
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+    picture = relationship('Picture', backref="transformed_pictures")
+
+
+class Comment(Base):
+    __tablename__ = "comments"
+    id = Column(Integer, primary_key=True)
+    text = Column(String(10000))
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+    picture_id = Column(Integer, ForeignKey(Picture.id, ondelete="CASCADE"))
+    user_id = Column(Integer, ForeignKey(User.id))
+    user = relationship('User', backref="comments")
+    picture = relationship('Picture', backref="comments")
