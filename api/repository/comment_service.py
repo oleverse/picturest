0fed30edfb8ca760645c74be158fa21725da3ebf
Dmiriy <mytoreh@gmail.com>
2023-07-24 16:42:21 +0300

    Comments
    трошки доробив

--- api/repository/comment_service.py
+++ api/repository/comment_service.py
@@ -12,8 +12,8 @@ def create_comment(db: Session, comment_data: CommentCreate, user_id: int, pictu
     return comment
 
 
-def update_comment(db: Session, comment_id: int, text: str):
-    comment = db.query(Comment).filter(Comment.id == comment_id).first()
+def update_comment(db: Session, comment_id: int, text: str, user_id: int):
+    comment = db.query(Comment).filter(Comment.id == comment_id, Comment.user_id == user_id).first()
     if not comment:
         return None
     comment.text = text
@@ -24,14 +24,14 @@ def update_comment(db: Session, comment_id: int, text: str):
     return comment
 
 
-def get_comment_by_id(db: Session, comment_id: int):
-    return db.query(Comment).filter(Comment.id == comment_id).first()
-
-
-def delete_comment(db: Session, comment_id: int):
-    comment = db.query(Comment).filter(Comment.id == comment_id).first()
+def delete_comment_by_id(db: Session, comment_id: int, user_id: int):
+    comment = db.query(Comment).filter(Comment.id == comment_id, Comment.user_id == user_id).first()
     if not comment:
         return None
     db.delete(comment)
     db.commit()
     return comment
+
+
+def get_comment_by_id(db: Session, comment_id: int):
+    return db.query(Comment).filter(Comment.id == comment_id).first()
--- api/routes/comments.py
+++ api/routes/comments.py
@@ -1,13 +1,15 @@
-from fastapi import APIRouter, Depends, HTTPException
+
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from api.database.db import get_db
 from api.repository.comment_service import (
-    create_comment, update_comment, get_comment_by_id, delete_comment
+    create_comment, update_comment, delete_comment_by_id, get_comment_by_id
 )
 from api.schemas import CommentCreate, CommentResponse
+from api.services.auth import get_current_user, Auth
 
 router = APIRouter()
-
+auth_service = Auth()
 
 @router.post("/comments/", response_model=CommentResponse)
 async def add_comment(comment_data: CommentCreate, user_id: int, picture_id: int, db: Session = Depends(get_db)):
@@ -16,26 +18,32 @@ async def add_comment(comment_data: CommentCreate, user_id: int, picture_id: int
         raise HTTPException(status_code=404, detail="Picture not found")
     return comment
 
-
 @router.put("/comments/{comment_id}", response_model=CommentResponse)
-async def edit_comment(comment_id: int, text: str, db: Session = Depends(get_db)):
-    comment = update_comment(db, comment_id, text)
+async def edit_comment(
+        comment_id: int,
+        text: str,
+        user=Depends(auth_service.get_current_user),
+        db: Session = Depends(get_db)
+):
+    comment = update_comment(db, comment_id, text, user.id)
     if not comment:
         raise HTTPException(status_code=404, detail="Comment not found")
     return comment
 
+@router.delete("/comments/{comment_id}", response_model=CommentResponse)
+async def delete_comment(
+        comment_id: int,
+        user=Depends(auth_service.get_current_user),
+        db: Session = Depends(get_db)
+):
+    comment = delete_comment_by_id(db, comment_id, user.id)
+    if not comment:
+        raise HTTPException(status_code=404, detail="Comment not found")
+    return comment
 
 @router.get("/comments/{comment_id}", response_model=CommentResponse)
 async def get_comment(comment_id: int, db: Session = Depends(get_db)):
     comment = get_comment_by_id(db, comment_id)
-    if not comment:
-        raise HTTPException(status_code=404, detail="Comment not found")
-    return comment
-
-
-@router.delete("/comments/{comment_id}", response_model=CommentResponse)
-async def remove_comment(comment_id: int, db: Session = Depends(get_db)):
-    comment = delete_comment(db, comment_id)
     if not comment:
         raise HTTPException(status_code=404, detail="Comment not found")
     return comment
