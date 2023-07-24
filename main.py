e6de9dafee8a2c79d67df1a9adb4dfd2966f4d23
Oleksii Petrashov <oleksii.petrashov@gmail.com>
2023-07-24 18:10:52 +0300

    Removing additional extra strings

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-from datetime import datetime
-
-import uvicorn as uvicorn
 from fastapi import FastAPI, Depends, HTTPException, Request, status
 from sqlalchemy.orm import Session
-
-from api.database.models import Comment, Picture, User
-from api.routes import pictures, comments
-
+from api.routes import pictures
 from sqlalchemy import text
 from api.database.db import get_db
+
 
 app = FastAPI()
 
@@ -31,7 +26,3 @@ def healthchecker(db: Session = Depends(get_db)):
 @app.get("/")
 async def root():
     return {"message": "Welcome!"}
-
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
