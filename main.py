a3233403341048107b3dc721ece29243fd86904f
Dmiriy <mytoreh@gmail.com>
2023-07-25 16:37:21 +0300

    Comments

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+import uvicorn
 from fastapi import FastAPI, Depends, HTTPException, Request, status
 from sqlalchemy.orm import Session
+
+
 from api.routes import pictures
 from sqlalchemy import text
 from api.database.db import get_db
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 
+
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 app.include_router(pictures.router, prefix='/api')
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/api/healthchecker")
@@ -17,18 +25,17 @@ def healthchecker(db: Session = Depends(get_db)):
         result = db.execute(text("SELECT 1")).fetchone()
         if result is None:
             raise HTTPException(status_code=500, detail="Database is not configured correctly")
-        return {"message": "Welcome to PictuREST!"}
+        return {"message": "Welcome to FastAPI!"}
     except Exception as e:
         print(e)
         raise HTTPException(status_code=500, detail="Error connecting to the database")
 
 
+# Головна сторінка
 @app.get("/")
-def root():
-    """
-    Default API route (i.e. / ) which does nothing
+async def root():
+    return {"Message": "Hello World"}
 
-    :return: Greeting string
-    :rtype: str
-    """
-    return {"message": "Welcome to PictuREST!"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
