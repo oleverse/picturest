e5bb3e8c792b513e9165f6e86848e9ca022b6fae
oleverse <123320101+oleverse@users.noreply.github.com>
2023-07-25 20:29:19 +0300

    Stick to the style, put db_session object to the end .

--- api/repository/tags.py
+++ api/repository/tags.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import exc
 from api.database.models import Tag, User, RoleNames
 
-async def create_tag(db: Session, tag_name: str):
+async def create_tag(tag_name: str, db: Session):
     existing_tag = db.query(Tag).filter(Tag.name == tag_name).first()
     if existing_tag is None:
         new_tag = Tag(name=tag_name)
