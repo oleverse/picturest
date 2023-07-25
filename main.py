5510bb64959be62f18bef741f286072561886c59
Dmiriy <mytoreh@gmail.com>
2023-07-25 13:51:33 +0300

    Web - Морда

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-
+import uvicorn
 from fastapi import FastAPI, Depends, HTTPException, Request, status
 from sqlalchemy.orm import Session
+
+
 from api.routes import pictures
 from sqlalchemy import text
 from api.database.db import get_db
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+
 
 from fastapi.templating import Jinja2Templates
 
@@ -26,10 +31,27 @@ def healthchecker(db: Session = Depends(get_db)):
         raise HTTPException(status_code=500, detail="Error connecting to the database")
 
 
-@app.get("/")
-async def root():
-    return {"message": "Welcome!"}
+# Головна сторінка
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
+# Сторінка реєстрації
+@app.get("/register", response_class=HTMLResponse)
+async def register_page(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
 
 
+# Сторінка логіну
+@app.get("/login", response_class=HTMLResponse)
+async def login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+# Завантаження світлини
+@app.get("/upload-photo", response_class=HTMLResponse)
+async def upload_page(request: Request):
+    return templates.TemplateResponse("upload.html", {"request": request})
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
