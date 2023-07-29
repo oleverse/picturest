c6051f3340e7b1bb2b75ab4e62ad1e78d00daef8
Nataliya2401 <sabotab2000@gmail.com>
2023-07-29 21:32:01 +0300

    Edit Put pictures
    Edit Put pictures - add tag to tags, which picture has

--- api/repository/pictures.py
+++ api/repository/pictures.py
@@ -1,14 +1,29 @@
 from typing import List, Type
 
 from sqlalchemy.orm import Session
+from fastapi import HTTPException
 
 from api.database.models import Picture, Tag, User
 from api.repository.tags import create_tag
 from api.schemas.essential import PictureCreate
 from api.services.cloud_picture import CloudImage
-
+from api.conf.config import settings
 
 async def create_picture(description: str, tags: List[str], file_path: str, db: Session, user: User):
+    """
+    The create_picture function creates a new picture in the database.
+        Args:
+            description (str): The description of the picture.
+            tags (List[str]): A list of tags for the picture.
+            file_path (str): The path to where the image is stored.
+
+    :param description: str: Set the description of the picture
+    :param tags: List[str]: Create a list of tags
+    :param file_path: str: Store the file path of the picture in the database
+    :param db: Session: Access the database
+    :param user: User: Get the user id from the database
+    :return: The picture object
+    """
     tags_list = []
 
     if tags:
@@ -66,13 +81,22 @@ async def remove_picture(picture_id: int, user: User, db: Session):
 async def update_picture(picture_id: int, body: PictureCreate, user: User, db: Session):
 
     picture = db.query(Picture).filter(Picture.id == picture_id).first()
-
     if picture:
-
         # TODO - кому дозволимо видаляти , наприклад - адмністратор може видаляти, що хоче, а юзер - свої
         # if user.role == admin or picture.user_id == user.id
+
         if picture.user_id == user.id:
-            tags_list = await transformation_list_to_tag(body.tags, db)
+            tag_names = []
+            if picture.tags:
+                for t in picture.tags:
+                    tag_names.append(t.name)
+                for el in body.tags:
+                    if el not in tag_names:
+                        tag_names.append(el)
+            if len(tag_names) > settings.max_tags:
+                raise HTTPException(status_code=400, detail=f"Too many tags. The maximum is {settings.max_tags}.")
+
+            tags_list = await transformation_list_to_tag(tag_names, db)
             picture.description = body.description
             picture.tags = tags_list
             picture.update = True
